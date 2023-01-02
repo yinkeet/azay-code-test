e9e6d1fb70e3c80f003bdc5e0a0a65c4a2916f16
yinkeet <wongyinkeet@gmail.com>
2023-01-02 10:40:27 +0800

    Added V3 which is disabling the array search

---
+++ php/app/src/Distributor/V3.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Azay\Distributor;
+
+class V3
+{
+	public static function distribute(array $fruit, $random_number_generator) {
+		while ($random_number_generator != 0) {
+			sort($fruit); //sort the array each time to get second(min) and min
+			$first = $fruit[0]['worker_after'];
+			$second = $fruit[1]['worker_after'];
+			$gap = ($second - $first) + 1;
+			if ($random_number_generator <= $gap) {
+	
+				$gap = $random_number_generator;
+			}
+			// $index = array_search(min($fruit), $fruit);
+			//echo $index."\r\n";
+			$fruit[0]['worker_after'] += $gap;
+			$fruit[0]['counter'] += $gap;
+	
+			$random_number_generator = $random_number_generator - $gap;
+		}
+		return $fruit;
+	}
+}
